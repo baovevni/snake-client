405f32856b0a6eacf79e06d5a1a7712c88275373
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-08 18:09:32 -0400

    added write username to the server

--- client.js
+++ client.js
@@ -5,12 +5,14 @@ const connect = function () {
     host: '165.227.47.243',
     port: 50541,
   });
-
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  })
+  conn.write("Name: BEE");
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   })
-
   return conn;
 };
 
--- play.js
+++ play.js
@@ -4,24 +4,3 @@ const connect = require("./client")
 
 console.log("Connecting ...");
 connect();
-
-
-
-
-
-
-
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//     port: 50541,
-//   });
-
-//   conn.setEncoding("utf8");
-//   conn.on("data", (data) => {
-//     console.log("Server says: ", data);
-//   })
-
-//   return conn;
-// };
